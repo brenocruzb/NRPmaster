4aade5bb8c97704f4459ca86b0a22ef50614b8cd
Breno Cruz <brenocruzb@gmail.com>
2015-02-01 23:14:31 -0200

    Desenvolvimento PSO
    Construção da Classe Particulas, e desenvolvimento da Classe PSO.
    Ainda falta desenvolver o método de cálculo da velocidade.

--- src/PSO/PSO.java
+++ src/PSO/PSO.java
@@ -1,5 +1,78 @@
 package PSO;
 
 public class PSO {
+	
+	private int tamPopulacao;
+	private Particula[] p;
+	
+//	private static final double V_MAX = 4.0;
+//	private static final double V_MIN = -4.0;
+	
+	public PSO(int numPopulacao, int dimensao){
+		this.tamPopulacao = numPopulacao;		
+		this.p = new Particula[numPopulacao];
+		
+		this.criarParticulas(dimensao);					
+	}
+	
+	private void criarParticulas(int dimensao){
+		this.p[0] = new Particula(dimensao);
+		for (int i = 1; i < this.p.length; i++){
+			this.p[i] = new Particula(dimensao);
+			
+			if(this.p[i-1].getPosicao().getSatisfacao() < this.p[i].getPosicao().getSatisfacao())
+				Particula.gBest = this.p[i].getPosicao();
+			else
+				Particula.gBest = this.p[i-1].getPosicao();
+		}
+	}
+	
+	public void avaliarParticulas(int criterioParada){
+		
+		for (int i = 0; i < this.p.length; i++) {
+			this.atualizarPosicao(this.p[i]);
+			this.atualizarLideres(this.p[i]);									
+		}						
+	}
+	
+	private double sigmoid(double velocidade){
+		return 1/(1 + Math.exp(-velocidade));
+	}
+	
+	private void atualizarPosicao(Particula p){
+		this.calcularVelocidade(p);	
+		for (int i = 0; i < this.p.length; i++) {
+			double s = this.sigmoid(p.getVelocidade()[i]);
+			if(Math.random() < s)
+				p.getPosicao().getSolucao()[i] = 1;
+			else
+				p.getPosicao().getSolucao()[i] = 0;										
+		}
+		
+		p.avalia();
+	}
+	
+	
+	private void calcularVelocidade(Particula p){
+		
+	}
+	
+	private void atualizarLideres(Particula p){
+		if(p.getPosicao().getSatisfacao() > p.getpBest().getSatisfacao())
+			p.setpBest(p.getPosicao());
+		
+		if(p.getPosicao().getSatisfacao() > Particula.gBest.getSatisfacao())
+			Particula.gBest = p.getPosicao();
+	}
+	
+	//Get Set
+
+	public int getTamPopulacao() {
+		return tamPopulacao;
+	}
+
+	public void setTamPopulacao(int tamPopulacao) {
+		this.tamPopulacao = tamPopulacao;
+	}
 
 }
--- src/PSO/Particula.java
+++ src/PSO/Particula.java
@@ -1,12 +1,100 @@
 package PSO;
 
+import java.util.ArrayList;
+
+import NRP.Cliente;
+import NRP.NRP;
+import NRP.Solucao;
+
 public class Particula {
 	
-	private int[] posicao;
-	private int[] velocidade;
+	public static Solucao gBest;
+	private Solucao pBest;	
 	
-	private int pBest;
+	private double[] velocidade;	
+	private Solucao posicao;
 	
-	public static int lBest;
+	private NRP nrp;
+	
+	public Particula(int dimensao){
+		this.posicao = new Solucao(nrp.getClientesTotal());
+		this.velocidade = new double[nrp.getClientesTotal()];
 
+		this.gerarSolucao(dimensao);
+		this.pBest = this.posicao; 
+	}
+	
+	private void gerarSolucao(int dimensao){					
+		
+		int[] solu = new int[this.posicao.tamanho()];
+		
+		for(int i = 0; i < this.posicao.tamanho(); i++) solu[i] = 0;
+		this.posicao.setSolucao(solu);
+		
+		for(int i = 0; i < this.posicao.tamanho(); i++){
+			
+			if(this.posicao.getCusto() < dimensao){
+				solu[i] = (Math.random() <= 0.5) ? 0 : 1;
+				if(solu[i] == 1){
+					this.posicao.setSolucao(solu);
+					this.avalia();
+				}
+				
+				if(this.posicao.getCusto() > dimensao){
+					solu[i] = 0;
+					this.posicao.setSolucao(solu);
+					this.avalia();
+				}
+			}else break;										
+		}
+		
+	}
+		
+	public void avalia(){
+		
+		ArrayList<Cliente> c = new ArrayList<>();
+		for (int i = 0; i < this.posicao.getSolucao().length; i++) {
+			if(this.posicao.getSolucao()[i] == 1){
+				c.add(this.nrp.getClientes().get(i));
+			}
+		}
+		
+		this.posicao.setCusto(this.nrp.getCustoDosClientes(c));
+		this.posicao.setSatisfacao(this.nrp.getSatisfacaoDosClientes(c));			
+	}
+
+	
+	//Get Set
+	
+	public Solucao getpBest() {
+		return pBest;
+	}
+
+	public void setpBest(Solucao pBest) {
+		this.pBest = pBest;
+	}
+
+	public Solucao getPosicao() {
+		return posicao;
+	}
+
+	public void setPosicao(Solucao posicao) {
+		this.posicao = posicao;
+	}
+
+	public double[] getVelocidade() {
+		return velocidade;
+	}
+
+	public void setVelocidade(double[] velocidade) {
+		this.velocidade = velocidade;
+	}
+
+	public NRP getNrp() {
+		return nrp;
+	}
+
+	public void setNrp(NRP nrp) {
+		this.nrp = nrp;
+	}
 }
