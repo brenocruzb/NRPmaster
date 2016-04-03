dcf24c53f37e5a55d89e787021af25a160794e63
Breno Cruz <brenocruzb@gmail.com>
2016-04-03 16:01:12 -0300

    PSO MuiltSwarm
    Atualização inciail para o desenvolvimento do PSO multiobjetivo

--- src/PSO/MainPSO.java
+++ src/PSO/MainPSO.java
@@ -15,42 +15,48 @@ public class MainPSO {
 		int dimensao = (int) ((30/100.0) * nrp.getCustoTotal());
 		int swarm = 10;
 		int populacao = 200/swarm;
-		int trocarGBest = 20;
 		
-		PSO[] pso = new PSO[swarm];
-		Solucao aux = new Solucao(nrp.getClientesTotal());
+		PSO pso = new PSO(nrp, populacao, dimensao);
+		pso.avaliarParticulas(20);
 		
-		for(int i = 0; i < swarm; i++){
-			pso[i] = new PSO(nrp, populacao, dimensao);			
-		}
+		System.out.println(Particula.getgBest().getSatisfacao());
 		
-		for(int j = 0; j < pso[0].gBest.getSolucao().length; j++)
-			System.out.print(j+ " ");
+		//int trocarGBest = 20;
 		
-		for(int i = 0; i < swarm; i++){
-			for(int j = 1; j <= 100; j++){
-				if(j % trocarGBest == 0){
-					
-					pso[i].avaliarParticulas(trocarGBest);
-					
-					aux.Clone(pso[0].gBest);		
-					for(int k = 0; k < pso.length - 1 ; k++){
-						pso[k].gBest.Clone(pso[k + 1].gBest);
-					}
-					pso[swarm - 1].gBest.Clone(aux);
-				}									
-			}			
-		} 
-	
-		for(int i = 0; i < swarm; i++){
-			System.out.println("=================");
-			System.out.println("GBest "+i+": ");
-			System.out.println("Custo: "+pso[i].gBest.getCusto());
-			System.out.println("Satisfação: "+pso[i].gBest.getSatisfacao());
-			for(int j = 0; j < pso[i].gBest.getSolucao().length; j++)
-				System.out.print(pso[i].gBest.getSolucao()[j]+" ");
-			System.out.println();
-		}
+		//PSO[] pso = new PSO[swarm];
+		//Solucao aux = new Solucao(nrp.getClientesTotal());
+		
+//		for(int i = 0; i < swarm; i++){
+//			pso[i] = new PSO(nrp, populacao, dimensao);			
+//		}
+		
+//		for(int j = 0; j < pso[0].gBest.getSolucao().length; j++)
+//			System.out.print(j+ " ");
+//		
+//		for(int i = 0; i < swarm; i++){
+//			for(int j = 1; j <= 100; j++){
+//				if(j % trocarGBest == 0){
+//					
+//					pso[i].avaliarParticulas(trocarGBest);
+//					
+//					aux.Clone(pso[0].gBest);		
+//					for(int k = 0; k < pso.length - 1 ; k++){
+//						pso[k].gBest.Clone(pso[k + 1].gBest);
+//					}
+//					pso[swarm - 1].gBest.Clone(aux);
+//				}									
+//			}			
+//		} 
+//	
+//		for(int i = 0; i < swarm; i++){
+//			System.out.println("=================");
+//			System.out.println("GBest "+i+": ");
+//			System.out.println("Custo: "+pso[i].gBest.getCusto());
+//			System.out.println("Satisfação: "+pso[i].gBest.getSatisfacao());
+//			for(int j = 0; j < pso[i].gBest.getSolucao().length; j++)
+//				System.out.print(pso[i].gBest.getSolucao()[j]+" ");
+//			System.out.println();
+//		}
 			
 	}
 }
